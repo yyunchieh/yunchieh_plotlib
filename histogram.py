b8299d7617ad33432ed0a808f701f22d2a3e974d
Jaslene <yunchien0102@gmail.com>
2024-10-16 11:16:22 +0800

    modify2

--- histogram.py
+++ histogram.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 def plot_histogram(data, column, bins=30, title="Histogram", xlabel = "Values", ylabel="Frequency"):
-    plt.figure(figsize=(10,6))
+    fig, ax = plt.subplots(figsize=(10,6))
     plt.hist(data[column], bins=bins, color = "skyblue", edgecolor = "white", alpha=0.7)
     plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
     plt.grid(axis="y", alpha=0.3)
     plt.show()
