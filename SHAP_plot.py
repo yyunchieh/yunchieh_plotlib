47956b4454568df02e5abaaa509d16757e943a38
Jaslene <yunchien0102@gmail.com>
2024-10-16 13:51:44 +0800

    update

--- SHAP_plot.py
+++ SHAP_plot.py
@@ -6,7 +6,10 @@ def plot_shap(model, data, title="SHAP Summary Plot"):
     shap_values = explainer.shap_values(data)
 
     for i in range(len(shap_values)):
-        plt.title(f"{title} - Class {i+1}")
+        fig, ax = plt.subplots(figsize=(10, 8))
+        ax.set_title(f"{title} - Class {i+1}")
         shap.summary_plot(shap_values[i], data, show=False)
-        plt.xticks(rotation=45)
+        ax.set_xticks(rotation=45)
+        ax.spines["right"].set_color("none")
+        ax.spines["top"].set_color("none")
         plt.show()
--- histogram.py
+++ histogram.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 
 def plot_histogram(data, column, bins=30, title="Histogram", xlabel = "Values", ylabel="Frequency"):
     fig, ax = plt.subplots(figsize=(10,6))
-    plt.hist(data[column], bins=bins, color = "skyblue", edgecolor = "white", alpha=0.7)
-    plt.title(title)
+    ax.hist(data[column], bins=bins, color = "skyblue", edgecolor = "white", alpha=0.7)
+    ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    plt.grid(axis="y", alpha=0.3)
+    ax.grid(axis="y", alpha=0.3)
+    ax.spines["right"].set_color("none")
+    ax.spines["top"].set_color("none")
     plt.show()
