b97bd83190187a3e863b3e5170428fd207a04dd8
Jaslene <yunchien0102@gmail.com>
2024-10-18 16:49:36 +0800

    update

--- visualization/histogram.py
+++ visualization/histogram.py
@@ -1,12 +1,32 @@
+#Histogram Function
 import matplotlib.pyplot as plt
 
-def plot_histogram(data, column, bins=30, title="Histogram", xlabel = "Values", ylabel="Frequency"):
-    fig, ax = plt.subplots(figsize=(10,6))
-    ax.hist(data[column], bins=bins, color = "skyblue", edgecolor = "white", alpha=0.7)
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+def plot_histogram(ax, data, column, bins=30, **kwargs):
+    ax.hist(data[column], 
+            bins=bins, 
+            color = "skyblue", 
+            edgecolor = "white", 
+            alpha=0.7)
+    
+    if 'set_title' in kwargs.keys():
+        ax.set_title(kwargs['set_title'])
+    
+    if 'set_xlabel' in kwargs.keys():
+        ax.set_xlabel(kwargs['set_xlabel'])
+         
+    if 'set_ylabel' in kwargs:
+        ax.set_ylabel(kwargs['set_ylabel'])
+
+    if 'set_xlim' in kwargs.keys():
+        ax.set_xlim(**kwargs['set_xlim'])
+        
+    if 'set_xticks' in kwargs.keys():
+        ax.set_xticks(**kwargs['set_xticks'])
+
+    if 'tick_params' in kwargs.keys():
+        ax.tick_params(**kwargs['tick_params'])
+
+   
     ax.grid(axis="y", alpha=0.3)
     ax.spines["right"].set_color("none")
     ax.spines["top"].set_color("none")
-    plt.show()
