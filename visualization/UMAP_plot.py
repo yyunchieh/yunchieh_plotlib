51a5b68f3bbe9cb2313dfa4e32f47692c0fe4df7
Jaslene <yunchien0102@gmail.com>
2024-10-21 15:33:45 +0800

    update

--- visualization/UMAP_plot.py
+++ visualization/UMAP_plot.py
@@ -1,23 +1,60 @@
+import numpy as np
+import pandas as pd
 import umap
+from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+from pandas.api.types import is_numeric_dtype
 
-def plot_umap(data, labels, title="UMAP Plot", cmap = "set2", random_state=42):
-    reducer = umap.UMAP(random_state=random_state)
-    X_umap = reducer.fit_transform(data)
+#UMAP_function
+
+def plot_umap(X, y, ax, **kwargs):
+
+    y = pd.Series(y) if not isinstance(y, pd.Series) else y
+    #Data Type
+    if is_numeric_dtype(y) and y.nunique() > 10 :
+        continuous = True
     
-    colors = cm.get_cmap(cmap, len(set(labels)))
+    else:
+        continuous = False
 
-    fig, ax = plt.subplots(figsize=(10, 8))
-    for category in set(labels):
-        idx = labels == category
-        ax.scatter(X_umap[idx, 0], X_umap[idx, 1], color=colors(category), label=f"class {category + 1}")
+    reducer = umap.UMAP()
+    embedding = reducer.fit_transform(X)
 
-    ax.legend(title="Classes")
-    ax.set_title(title)
-    ax.set_xlabel("UMAP dimension 1")
-    ax.set_ylabel("UMAP dimension 2")
+    plt.sca(ax)
+
+    #color bar
+    if continuous:
+        sc = ax.scatter(embedding[:, 0], embedding[:, 1], c=y, cmap='viridis', s=kwargs.get('s',50),alpha=0.7)
+        colorbar = plt.colorbar(sc, ax=ax)
+        colorbar.set_label('Continuous value')
+
+    #legend
+    else:
+        unique_classes = np.unique(y)
+        for i, cls in enumerate(unique_classes):
+            ax.scatter(embedding[y == cls, 0], embedding[y == cls, 1], label = f"Class{cls}", s=kwargs.get('s',50),alpha=0.7)
+        ax.legend(title="Classes", loc="best", fontsize=10)
+
+
+
+    if 'set_title' in kwargs.keys():
+        ax.set_title(kwargs['set_title']["label"])
+    
+    if 'set_xlabel' in kwargs.keys():
+        ax.set_xlabel(kwargs['set_xlabel'])
+
+    if 'set_ylabel' in kwargs.keys():
+        ax.set_label(kwargs['set_ylabel'])
+
+    if 'set_xlim' in kwargs.keys():
+        ax.set_xlim(kwargs['set_xlim'])
+        
+    if 'set_xticks' in kwargs.keys():
+        ax.set_xticks(kwargs['set_xticks'])
+        
+    if 'tick_params' in kwargs.keys():
+        ax.tick_params(**kwargs['tick_params'])
+
+    
     ax.spines["right"].set_color("none")
     ax.spines["top"].set_color("none")
-
-    plt.show()
--- visualization/tSNE_plot.py
+++ visualization/tSNE_plot.py
@@ -1,43 +1,57 @@
 from sklearn.manifold import TSNE
 from sklearn.datasets import load_iris
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from pandas.api.types import is_numeric_dtype
+
 
 #t-SNE function
 
-def plot_tsne(tsne_results, ax, **kwargs):
+def plot_tsne(X, y, ax, **kwargs):
+    y = pd.Series(y) if not isinstance(y, pd.Series) else y
+    #Data Type
+    if is_numeric_dtype(y) and y.nunique() > 10:
+        continuous = True
+
+    else:
+        continuous = False
+
+
+    tsne = TSNE(n_components=2, random_state=0)
+    embedding = tsne.fit_transform(X)
+
 
     plt.sca(ax)
 
-    scatter_params = kwargs.get("scatter_params", {})
-    scatter = ax.scatter(tsne_results[:, 0], tsne_results[:, 1], **scatter_params)
+    #color bar
+    if continuous:
+        sc = ax.scatter(embedding[:, 0], embedding[:, 1], c=y, cmap="viridis", s=kwargs.get("s",50), alpha=0.7)
+        colorbar = plt.colorbar(sc, ax=ax)
+        colorbar.set_label("Continuous Value")
+
+    #legend
+    else:
+        unique_classes = np.unique(y)
+        for i, cls in enumerate(unique_classes):
+            ax.scatter(embedding[y == cls, 0], embedding[y == cls, 1], label=f"Class{cls}", s=kwargs.get("s",50), alpha=0.7)
+        ax.legend(title="Classes", loc="best", fontsize=10)
+
  
     if "set_title" in kwargs.keys():
-        ax.set_title(**kwargs["set_title"])
+        ax.set_title(kwargs["set_title"]["label"])
 
     if "set_xlabel" in kwargs.keys():
-        ax.set_xlabel(**kwargs["set_xlabel"])
+        ax.set_xlabel(kwargs["set_xlabel"])
 
     if "set_xlim" in kwargs.keys():
-        ax.set_xlim(**kwargs["set_xlim"])
+        ax.set_xlim(kwargs["set_xlim"])
         
     if "set_xticks" in kwargs.keys():
-        ax.set_xticks(**kwargs["set_xticks"])
+        ax.set_xticks(kwargs["set_xticks"])
 
     if "tick_params" in kwargs.keys():
         ax.tick_params(**kwargs["tick_params"])
 
     ax.spines["right"].set_color("none")
     ax.spines["top"].set_color("none")
-
-    
-    if kwargs.get("color_bar", False):
-        colorbar_ax = plt.colorbar(scatter, ax=ax, format='{x:.1f}')
-        vmin, vmax = np.min(colorbar_ax.get_ticks()), np.max(colorbar_ax.get_ticks())
-        colorbar_ax.mappable.set_clim(vmin=vmin, vmax=vmax)
-        colorbar_ax.set_ticks(ticks = [vmin, vmax], labels=['Low', 'High'], size = 12)
-        colorbar_ax.set_label(label='Feature value', size = 16, labelpad=20, y=0.5)
-        colorbar_ax.outline.set_visible(False)
-        colorbar_ax.ax.tick_params(axis='both', top=False, right=False, bottom=False, left=False)
-    
