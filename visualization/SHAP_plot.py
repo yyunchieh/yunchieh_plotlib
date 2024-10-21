4c9fce1ac017052bed76d3532ec198c178f5f313
Jaslene <yunchien0102@gmail.com>
2024-10-21 15:21:31 +0800

    update

--- visualization/SHAP_plot.py
+++ visualization/SHAP_plot.py
@@ -40,18 +40,3 @@ def plot_shap(shap_values,ax, color_bar, **kwargs):
         colorbar_ax.outline.set_visible(False)
         colorbar_ax.ax.tick_params(axis='both', top=False, right=False, bottom=False, left=False)
 
-
-
-#previous
-#plot_shap(model, data, title="SHAP Summary Plot"):
- #   explainer = shap.TreeExplainer(model)
-  #  shap_values = explainer.shap_values(data)
-
-   # for i in range(len(shap_values)):
-    #    fig, ax = plt.subplots(figsize=(10, 8))
-     #   ax.set_title(f"{title} - Class {i+1}")
-     #  shap.summary_plot(shap_values[i], data, show=False)
-     #  ax.set_xticks(rotation=45)
-     #   ax.spines["right"].set_color("none")
-     #   ax.spines["top"].set_color("none")
-     #   plt.show()
