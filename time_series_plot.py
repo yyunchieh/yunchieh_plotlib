b1294b56abfa4bd2e6e99f623ce74452201f69cd
Jaslene <yunchien0102@gmail.com>
2024-10-21 10:44:32 +0800

    update

---
+++ time_series_plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+def plot_time_series(data, x_column, y_column, title = "Times Series Plot", xlabel = "Timestamp", ylabel = "Value", legend_label = "Value"):
+    fig,ax = plt.subplots(figsize=(12,6))
+    plt.plot(data[x_column], data[y_column], label = legend_label, color = "blue")
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.xticks(rotation=45)
+    ax.grid(axis="y",alpha=0.3)
+    ax.legend(loc="best")
+    ax.spines["right"].set_color("none")
+    ax.spines["top"].set_color("none")
+    plt.show()
